b3ed9846150c017ac21269103defe2af6606ee44
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-30 10:10:13 +1200

    ScrollViewExtension

---
+++ BankingApp/UIComponents/ScrollView+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  ScrollView+Extensions.swift
+//  BankingApp
+//
+//  Created by Nikita Shvad on 30.08.2023.
+//  Copyright © 2023 Nikita Shvadlenko. All rights reserved.
+//
+
+import UIKit
+
+extension UIScrollView {
+    func scrollToPage(pageNumber: Int) {
+        let nextVisible = CGRect(
+            x: frame.size.width * CGFloat(pageNumber),
+            y: frame.origin.y,
+            width: frame.size.width,
+            height: frame.size.height
+        )
+        scrollRectToVisible(nextVisible, animated: true)
+    }
+}
