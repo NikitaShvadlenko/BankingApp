adda1255bf972ec65dc414103e2eceadd8c4e0f1
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-09-09 17:07:12 +1200

    wip

--- BankingApp/Feature/ServicesScreen/OpenAccount/OpenAccountView.swift
+++ BankingApp/Feature/ServicesScreen/OpenAccount/OpenAccountView.swift
@@ -9,23 +9,25 @@
 import UIKit
 
 class OpenAccountView: UIView {
-     lazy var titleLabel: UILabel = {
+    lazy var titleLabel: UILabel = {
         let titleLabel = UILabel()
         titleLabel.font = UIFont.systemFont(ofSize: 26)
+        titleLabel.adjustsFontSizeToFitWidth = true
+        titleLabel.minimumScaleFactor = 0.5
         titleLabel.textColor = Asset.Colors.applicationFormLabel.color
         titleLabel.textAlignment = .natural
         return titleLabel
     }()
-
+    
     lazy var pageView = OpenAccountPageView()
-
+    
     lazy var contentView = UIView()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -54,13 +56,13 @@ extension OpenAccountView {
             make.top.equalTo(safeAreaLayoutGuide).inset(20)
             make.leading.trailing.equalToSuperview().inset(14)
         }
-
+        
         pageView.snp.makeConstraints { make in
             make.height.equalTo(44)
             make.leading.trailing.equalToSuperview()
             make.top.equalTo(titleLabel.snp.bottom).offset(14)
         }
-
+        
         contentView.snp.makeConstraints { make in
             make.top.equalTo(pageView.snp.bottom).inset(-20)
             make.leading.trailing.bottom.equalToSuperview()
